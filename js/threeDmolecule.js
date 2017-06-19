d2efb61045d21f50549dff0be2bfef898ac8a8da
Joe Fields <fieldsj1@southernct.edu>
2017-06-19 17:14:49 -0400

    "resize" and "fit" fixes

--- js/threeDmolecule.js
+++ js/threeDmolecule.js
@@ -88,16 +88,11 @@
      
 
       self.onWindowResize = function () {
-
-
         self.camera.aspect = 4/3;
-        self.camera.updateProjectionMatrix();
-        var w = self.container.clientWidth;
+        var w = self.container.clientWidth-4;
         var h = .75*w;
-        //self.renderer.setSize( 700, 525);
         self.renderer.setSize( w, h);
-        //self.renderer.setSize( 400, 300);
-        //self.container.setSize(w, h);
+        self.camera.updateProjectionMatrix();
       };
 
       self.animate = function() {
